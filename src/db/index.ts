c5146bff2cf215530aa4b7aad5a3a77ad776b2cf
Mariasiu Vlad <mariasiuvlad@gmail.com>
2021-03-21 12:16:40 +0200

    Update get events query

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,6 @@ import { Pool } from "pg";
 const pool = new Pool();
 
 export const query = (text: string, params: any[]) => pool.query(text, params);
-export const GET_EVENTS = "SELECT * FROM schedule WHERE timestamp >= $1";
+export const GET_EVENTS =
+  "SELECT * FROM schedule WHERE timestamp >= $1 or timestamp IS NULL";
 export const getEvents = () => query(GET_EVENTS, [new Date(Date.now())]);
